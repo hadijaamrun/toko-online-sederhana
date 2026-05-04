411c529c79a34b58d4f1096c22acff54c981f589
hadijaamrun <hanifnurmaajid@outlook.com>
2026-05-04 08:18:54 +0700

    analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { CartProvider } from './context/CartContext.jsx';
-import { AuthProvider } from './context/AuthContext.jsx'; 
+import { AuthProvider } from './context/AuthContext.jsx';
 import './styles/global.css';
+import ReactGA from "react-ga4"; 
+
+ReactGA.initialize("G-TSDGQTX11Z"); 
+
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>    
+      <AuthProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
